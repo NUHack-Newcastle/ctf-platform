ec947af2aeda76301acb1e53265dca89e658316e
CaptureTheFrog <capturethefrog@proton.me>
2024-03-11 02:25:35 +0000

    Add icon property to challenge categories

--- models/challengecategory.py
+++ models/challengecategory.py
@@ -1,15 +1,19 @@
 import os
 import json
-from typing import Set
+import sys
+from typing import Set, Optional
 
 from models.challenge import Challenge
+from models.icon import Icon
+from models.icon_bootstrap import IconBootstrap
 
 
 class ChallengeCategory:
-    def __init__(self, slug: str, name: str):
+    def __init__(self, slug: str, name: str, icon: Optional[Icon] = None):
         self.__slug: str = slug
         self.__name: str = name
         self.__challenges: Set[Challenge] = set()
+        self.__icon: Optional[Icon] = icon
 
     @property
     def name(self) -> str:
@@ -23,6 +27,10 @@ class ChallengeCategory:
     def challenges(self) -> Set[Challenge]:
         return self.__challenges.copy()
 
+    @property
+    def icon(self) -> Optional[Icon]:
+        return self.__icon
+
     def add_challenge(self, challenge: Challenge):
         self.__challenges.add(challenge)
 
@@ -35,4 +43,16 @@ class ChallengeCategory:
         name = f.read()
         f.close()
 
-        return ChallengeCategory(slug=os.path.split(directory)[-1], name=name)
+        icon: Optional[Icon] = None
+        if os.path.exists(os.path.join(directory, 'icon')):
+            if os.path.islink(os.path.join(directory, 'icon')):
+                realpath = os.path.realpath(os.path.join(directory, 'icon'))
+                if os.path.basename(realpath) == 'icon_bootstrap':
+                    f = open(realpath, 'r')
+                    icon = IconBootstrap(f.read())
+                    f.close()
+                else:
+                    sys.stderr.write("Warning: category icon is link to unknown name, not sure how to proceed. Omitting\n")
+            else:
+                sys.stderr.write("Warning: category icon is not link, not sure how to proceed. Omitting\n")
+        return ChallengeCategory(slug=os.path.split(directory)[-1], name=name, icon=icon)
