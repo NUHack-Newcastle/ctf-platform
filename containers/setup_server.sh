6c8b1bd6ac8f4ce5b780a48b463343dca5de4d5c
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 21:02:23 +0000

    Fix docker install script

--- containers/setup_server.sh
+++ containers/setup_server.sh
@@ -4,10 +4,10 @@
 sudo apt update -y
 sudo apt install curl apt-transport-https ca-certificates  -y
 sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) \
-signed-by=/usr/share/keyrings/docker.gpg] \
+signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update -y
