10baff37f1302a639c26aa4275c705d6f8fa9f4b
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 21:36:11 +0000

    Add nginx to docker install script

--- containers/setup_server.sh
+++ containers/setup_server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # source: https://earthly.dev/blog/private-docker-registry/
 sudo apt update -y
 sudo apt install curl apt-transport-https ca-certificates  -y
@@ -13,3 +15,27 @@ https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 sudo apt update -y
 sudo apt install docker-ce docker-compose -y
 docker --version || { >&2 echo "Failed to install docker" && exit 1; }
+
+mkdir "$SCRIPT_DIR"/registry
+mkdir "$SCRIPT_DIR"/registry/registry-data
+
+cd "$SCRIPT_DIR"/registry
+cp "$SCRIPT_DIR"/docker-compose.yml "$SCRIPT_DIR"/registry
+
+sudo apt install nginx apache2-utils -y
+sudo cp "$SCRIPT_DIR"/registry.conf /etc/nginx/conf.d/registry.conf
+
+directives_to_add="
+    client_max_body_size 4000m;
+    server_names_hash_bucket_size 64;
+"
+
+if ! sudo grep -q "client_max_body_size" /etc/nginx/nginx.conf; then
+    sudo sed -i "/^http {/a\\${directives_to_add}" /etc/nginx/nginx.conf
+    echo "Directives added to nginx.conf"
+else
+    echo "Directives already exist in nginx.conf"
+fi
+
+sudo systemctl restart nginx
+sudo systemctl status nginx
