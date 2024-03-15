9c81635945ce1827e9529cced57fa9288c49550f
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 20:56:04 +0000

    Fix docker install script

--- containers/setup_server.sh
+++ containers/setup_server.sh
@@ -6,7 +6,7 @@ sudo apt install curl apt-transport-https ca-certificates  -y
 echo "deb [arch=$(dpkg --print-architecture) \
 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
 https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-| tee /etc/apt/sources.list.d/docker.list
+| sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update -y
 sudo apt install docker-ce docker-compose -y
 docker --version || { >&2 echo "Failed to install docker" && exit 1; }
