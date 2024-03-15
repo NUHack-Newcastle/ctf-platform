651ecd715e42ec4e2f968eb5544a28043af15163
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 12:09:09 +0000

    Add multiplier to solve

--- models/solve.py
+++ models/solve.py
@@ -13,6 +13,7 @@ class Solve(db.Model):
     __user_column = db.Column('user', db.String(32), db.ForeignKey('user.username'), nullable=False)
     __challenge_column = db.Column('challenge', db.String(128), nullable=False)
     when = db.Column(db.DateTime, nullable=False)
+    multiplier = db.Column(db.Integer, nullable=False, default=1)
 
     __table_args__ = (
         db.PrimaryKeyConstraint(
@@ -20,11 +21,12 @@ class Solve(db.Model):
         ),
     )
 
-    def __init__(self, team: 'Team', user: 'User', challenge: 'Challenge', when: datetime):
+    def __init__(self, team: 'Team', user: 'User', challenge: 'Challenge', when: datetime, multiplier: int = 1):
         self.__team = team
         self.__user = user
         self.__challenge_column = challenge.slug
         self.when = when
+        self.multiplier = multiplier
 
     @property
     def challenge(self) -> 'Challenge':
